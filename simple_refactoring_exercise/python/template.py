5e1e40d1cab7a450228ac6c14bc765ede654ab0d
Jeff Falberg <auronzeroo@aim.com>
2017-06-05 16:41:16 -0400

    Completed the `simple_refractoring_exercise`

--- simple_refactoring_exercise/python/template.py
+++ simple_refactoring_exercise/python/template.py
@@ -1,21 +1,18 @@
 def template(source_template, req_id):
-    
-    template = str(source_template)
+	import re
+	template = str(source_template)
 
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
-    code = str(req_id)
-    template = str(template_part_one + code + template_part_two)
+	## Substitute for %CODE%#
+	code = str(req_id)
+	template = re.sub("%CODE%",code,template)
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
-    altcode = code[0:5] + "-" + code[5:8]
-    return template_part_one + altcode + template_part_two
+	## Substitute for %ALTCODE%##
+	altcode = code[0:5] + "-" + code[5:8]
+	template = re.sub("%ALTCODE%", altcode, template)
+
+	return template
   
 
+# Using indices to locate the variables in the string is unnecessary and confusing to anyone reading the original code. 
+# With the re.sub function, the %CODE% and %ALTCODE% variables can replace in only one line each.
+# Not only does this look simpler to understand, it also involves fewer variable names and lines of code.
