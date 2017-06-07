0190b472aa0b5493669b9f692702fdf770fc0ec0
Farhat Nader <farhatmnader@gmail.com>
2017-06-07 02:51:14 -0400

    refactored template

--- simple_refactoring_exercise/python/template.py
+++ simple_refactoring_exercise/python/template.py
@@ -3,19 +3,24 @@ def template(source_template, req_id):
     template = str(source_template)
 
     # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
     code = str(req_id)
-    template = str(template_part_one + code + template_part_two)
+    template = template.replace("%CODE%", code)
 
     # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
     altcode = code[0:5] + "-" + code[5:8]
-    return template_part_one + altcode + template_part_two
-  
+    template = template.replace("%ALTCODE%", altcode)
 
+    return template
+
+
+'''
+One thing I noticed was the ignoring of standard library methods,
+    such as manually creating split versions of the string, 
+    when there exits the .split() method.
+I initially thought of using .split() with .join(), for example:
+    template = (code).join(template.split("%CODE%"))
+But decided that .replace() was straightforward, used less code, 
+    and tells the reader exactly what it's doing.
+
+I also removed the redundant/repititive use of str()
+'''
