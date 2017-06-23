d1cc4c332833bc74b9ccd87d5bd0f36516d7fbe1
Rainer Arencibia <rainer85ah@gmail.com>
2017-06-22 23:20:35 -0400

    Uploading Data Engineer jobs
    Uploading solutions for two exerscises.

--- simple_refactoring_exercise/python/template.py
+++ simple_refactoring_exercise/python/template.py
@@ -1,21 +1,12 @@
 def template(source_template, req_id):
-    
-    template = str(source_template)
+    template_str = str(source_template)
 
     # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
     code = str(req_id)
-    template = str(template_part_one + code + template_part_two)
+    template_str = template_str.replace("%CODE%", code)
 
     # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
     altcode = code[0:5] + "-" + code[5:8]
-    return template_part_one + altcode + template_part_two
-  
+    template_str = template_str.replace("%ALTCODE%", altcode)
 
+    return template_str
