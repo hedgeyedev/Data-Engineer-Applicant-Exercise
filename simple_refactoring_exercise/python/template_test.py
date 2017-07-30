de22470d9bb4ce5d82af65796c37a328416aa7ff
sammsteinfeld <30530796+sammsteinfeld@users.noreply.github.com>
2017-07-30 16:15:56 -0400

    Update template_test.py

--- simple_refactoring_exercise/python/template_test.py
+++ simple_refactoring_exercise/python/template_test.py
@@ -1,13 +1,19 @@
 import unittest
 from template import template
  
-class TestTemplate(unittest.TestCase):
+#I initialized the variables in the class instead so they can be reused
  
+class TestTemplate(unittest.TestCase):
+    
     def setUp(self):
-        pass
+        self.start = 5 #start of string
+        self.end = 8 #end of string
+        self.code = "%CODE%"
+        self.altcode = "%ALTCODE%"
+    
  
     def test_substitute_code_and_altcode(self):
-        self.assertEqual( template('Code is %CODE%; alt code is %ALTCODE%', '5678901234'), 'Code is 5678901234; alt code is 56789-012')
+        self.assertEqual( template('Code is %CODE%; alt code is %ALTCODE%', '5678901234', self.start, self.end, self.code, self.altcode), 'Code is 5678901234; alt code is 56789-012')
  
  
 if __name__ == '__main__':
