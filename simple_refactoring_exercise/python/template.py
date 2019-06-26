5546f5cc4be9a39763f7f9e601824d1d3036a963
Adedeji Ayinde <ayindedeji@gmail.com>
2019-06-26 19:14:16 -0400

    notes refactored template
    *A function called parts() was created to abstract the repetitive process of creating template_part_one & template_part_two
    
    *template_part_one & template_part_two were renamed to part1 and part2 respectively
    *and the new function parts() is invoke when needed the parts in the body of the code

--- simple_refactoring_exercise/python/template.py
+++ simple_refactoring_exercise/python/template.py
@@ -1,21 +1,24 @@
 def template(source_template, req_id):
     
     template = str(source_template)
+    
+    def parts(temp):
+        return str(temp[0:(template_split_begin)]), str(temp[template_split_end:len(temp)])
 
+    
     # Substitute for %CODE%
     template_split_begin = template.index("%CODE%")
     template_split_end = template_split_begin + 6
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
+    part1,part2 = parts(template)
     code = str(req_id)
-    template = str(template_part_one + code + template_part_two)
+    template = str(part1 + code + part2)
 
     # Substitute for %ALTCODE%
     template_split_begin = template.index("%ALTCODE%")
+    template_split_begin = template.index('%ALTCODE%')
     template_split_end = template_split_begin + 9
-    template_part_one = str(template[0:(template_split_begin)])
-    template_part_two = str(template[template_split_end:len(template)])
+    part1,part2 = parts(template)
     altcode = code[0:5] + "-" + code[5:8]
-    return template_part_one + altcode + template_part_two
+    return part1 + altcode + part2
   
 
